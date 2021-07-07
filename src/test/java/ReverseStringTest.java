47991b40a8b3914a5e9fc9b16dc9ee19be475982
Angelica <Angelica137@users.noreply.github.com>
2021-07-07 21:07:26 +0100

    .reverse() returns a string, red

--- src/test/java/ReverseStringTest.java
+++ src/test/java/ReverseStringTest.java
@@ -1,3 +1,12 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
 public class ReverseStringTest {
+
+	@Test
+	public void testReverseStringReturnsString() {
+		assertEquals("Hello, world!", new ReverseString().reverse());
+	}
 
 }
