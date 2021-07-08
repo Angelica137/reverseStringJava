d6a892bf38b4e2cacb72e6e1f9bc9f4228b43c2a
Angelica <Angelica137@users.noreply.github.com>
2021-07-08 23:04:15 +0100

    delete obsolete

--- src/main/java/ReverseString.java
+++ src/main/java/ReverseString.java
@@ -1,14 +1,9 @@
 public class ReverseString {
 
 	public String reverse(String str) {
-		// create an array of length str.length()
 		char[] charArr = new char[str.length()];
-		// create an index var i
 		int i = 0;
-		// have an index variable j == length of string - 1,
-		// so I can use it as an index
 		int j = str.length() - 1;
-		// check if str.length() is odd
 		if (str.length() % 2 == 0) {
 			while (i < str.length() / 2) {
 				charArr[i] = str.charAt(j);
@@ -33,16 +28,6 @@ public class ReverseString {
 			return answer;
 		}
 
-	}
-
-	public static void main(String[] args) {
-		String str = "123456";
-		ReverseString r = new ReverseString();
-		r.reverse(str);
-
-		String str2 = "12345";
-		ReverseString r2 = new ReverseString();
-		r2.reverse(str2);
 	}
 
 }
